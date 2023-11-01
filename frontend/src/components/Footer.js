fc279639968515ff057896e6a4a972fe07b50db7
Landon Crabtree <me@landon.pw>
2023-11-01 17:51:39 -0500

    fixed footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -21,10 +21,10 @@ function Footer() {
             <a href="/#about">About Us</a>
             </li>
             <li>
-            <a href="der-data">About Us</a>
+            <a href="der-data">DER Data</a>
             </li>
             <li>
-            <a href="/#contact">About Us</a>
+            <a href="/#contact">Contact Us</a>
             </li>
           </ul>
         </div>
